8f72b237295fe92bc762fe3478017d44318617bb
Agata Kamińska <a.fluffypie@gmail.com>
2024-05-24 13:10:50 +0200

    added return

--- script.js
+++ script.js
@@ -4,9 +4,14 @@ W tym zadaniu musisz wykorzystać już wybrany dla Ciebie element (węzeł DOM),
  */
 
 function colorizeElement(node) {
+  if (!node.tagName) {
+    console.error("Nie znaleziono elementu w drzewie DOM");
+    return;
+  }
   node.classList.add("new-class");
   let tagName = node.tagName.toLowerCase();
   node.setAttribute("data-tag", tagName);
   node.style.color = "#f1f1f1";
   node.style.fontSize = "12px";
+  return node.outerHTML;
 }
