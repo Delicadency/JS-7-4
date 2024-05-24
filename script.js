ce8c4ad9a338e8aaf316ab3dbf5610d4e23fa61f
Agata Kamińska <a.fluffypie@gmail.com>
2024-05-24 13:11:59 +0200

    deleted comment

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-/* Stwórz funkcję colorizeElement, która będzie zmieniała stylowanie przekazanego w parametrze elementu. Funkcja może otrzymać dowolny element HTML w parametrze. Funkcja powinna dodawać do elementu klasę new-class, atrybut data-tag przechowujący nazwę tagu otrzymanego w parametrze, oraz powinna ustawić kolor tekstu w tym elemencie na #f1f1f1 i wielkość czcionki na 12px. Na koniec, funkcja powinna zwrócić (return) zmodyfikowany element.
-
-W tym zadaniu musisz wykorzystać już wybrany dla Ciebie element (węzeł DOM), który funkcja ma otrzymać przekazany, jako parametr w inpucie. Nie ma wobec tego potrzeby robić samemu document.querySelector("#test"), wystarczy jedynie function(node) { // i tutaj użycie node (pod którym kryje się <div id="test"></div>).
- */
-
 function colorizeElement(node) {
   if (!node.tagName) {
     console.error("Nie znaleziono elementu w drzewie DOM");
